54547c158ad57256e4316df1d31fd2a8ac2493e3
vxnuaj <jv.100420@gmail.com>
2024-08-17 15:03:22 -0700

    GradientBoosting

--- 02-classification/Ensembles/gradientboost.py
+++ 02-classification/Ensembles/gradientboost.py
@@ -4,39 +4,87 @@ from nue.preprocessing import csv_to_numpy, x_y_split, train_test_split
 '''
 TODO: 
 
-- [ ] build gradient boosting / decision tree with classification
-- [ ] build graidnet boosting / decision tree with regression
-
+if time do regression if not stick with classification
 '''
 
+class GradientBoost:
+    def __init__(self, verbose_train = False, verbose_test = False):
+        self.verbose_train = verbose_train
+        self.verbose_test = verbose_test
+        self.trees = [ ]
+    
+    def train(self, X_train, Y_train, n_trees = 50, alpha = .01, modality = 'b-classification', dtree_dict = None):
+        self.X_train = X_train
+        self.Y_train = Y_train
+        self.n_trees = n_trees
+        self.alpha = alpha
+        self.modality = modality
+        self.all_raw_preds = []
+        self.dtree_dict = dtree_dict
+        self._get_dict()
+        self._train_trees()
 
-class Node:
-    def __init__(self, value = None, Y = None, feature = None, threshold = None, left_node = None, right_node = None, is_root = False, depth = 0, is_base = False):
-        self.value = value
-        self.Y = Y
-        self.feature = feature
-        self.threshold = threshold
-        self.left_node = left_node
-        self.right_node = right_node
-        self.is_base = is_base
+    def _train_trees(self):
+        preds = self._log_probs(self.Y_train) # raw unactivated output.
+        probs = self._sigmoid(preds) # output as a probability
+        residual = self._residual(self.Y_train, probs) # residual
+        print(residual)
+        for i in range(self.n_trees):
+            tree = DecisionTree(**self._init_dict)
+            tree.train(self.X_train, residual, modality = self.modality, **self._train_dict)
 
-    def _is_leaf(self):
-        return self.value is not None
+            tree_preds = tree.test(self.X_train)
+            preds += self.alpha * tree_preds
+
+            probs = self._sigmoid(preds)
+
+            if self.verbose_train:
+                loss = self._bce(self.Y_train, probs)
+                acc = self._accuracy(self.Y_train, probs)
+                print(f"TREE #{i} | ACCURACY: {acc}% | LOSS: {loss}")
+
+            residual = self._residual(self.Y_train, probs)
+            self.trees.append(tree)
+
+    def _log_probs(self, Y):
+        labels, counts = np.unique(Y, return_counts = True)
+        probs = counts / Y.size
+        return probs[1]
+
+    def _residual(self, Y, prob):
+        return Y - prob
+
+    def _sigmoid(self, logit):
+        return 1 / (1 + np.exp(-logit))
+
+    def _bce(self, Y, prob):
+        return - np.mean(Y * np.log(prob) + ( 1 - Y ) * np.log(1 - prob))
+
+    def _accuracy(self, Y, prob):
+        print(prob[:10])
+        pred = np.round(prob, decimals = 0)
+        print(pred[:10])
+        return np.mean(Y.flatten() == pred.flatten()) * 100
+
+    def _get_dict(self):
+        self._init_dict = {k:v for k,v in self.dtree_dict.items() if k in ['verbose_train', 'verbose_test']}
+        self._train_dict = {k:v for k,v in self.dtree_dict.items() if k in ['criterion', 'max_depth', 'min_node_samples']}
 
 class DecisionTree:
-    def __init__(self, verbose_train, verbose_test):
+    def __init__(self, verbose_train = False, verbose_test = False):
         self.verbose_train = verbose_train
         self.verbose_test = verbose_test
         self.root = None
         self.n_leaf = 0
 
-    def train(self, X_train, Y_train, modality = 'classification', criterion = 'gini', max_depth = 8, min_node_samples = 2):
+    def train(self, X_train, Y_train, modality = 'b-classification', criterion = 'gini', max_depth = 8, min_node_samples = 2):
         self.X_train = X_train
         self.Y_train = Y_train
         self.modality = modality
         self.criterion = criterion
         self.max_depth = max_depth
         self.min_node_samples = min_node_samples
+
         '''
         note that for regression, the criterion has to be mse or mae. 
         for classificaiton, can be entropy or gini
@@ -48,10 +96,13 @@ class DecisionTree:
         self.Y_test = Y_test
 
         self.preds = np.array([self._traverse(x) for x in self.X_test])
-        if self.Y_test.any():
+        
+        if self.Y_test is not None:
             acc = self._accuracy(self.Y_test, self.preds)
             if self.verbose_test:
                 print(f"\nAccuracy: {acc}%")
+
+        return self.preds
 
     def _grow_tree(self, X, Y, depth = 0):
         n_samples, n_features = X.shape
@@ -108,7 +159,7 @@ class DecisionTree:
         left_idxs, right_idxs = self._split(X_col, thresh)
         
         if len(left_idxs) == 0 or len(right_idxs) == 0:
-            return 0
+            return -1000
         
         n = len(Y)
         n_l = len(left_idxs)
@@ -152,44 +203,38 @@ class DecisionTree:
                 node = node.right_node
         return node.value
 
-class GradientBoost:
-    def __init__(self, verbose_train, verbose_test):
-        self.verbose_train = verbose_train
-        self.verbose_test = verbose_test
+class Node:
+    def __init__(self, value = None, Y = None, feature = None, threshold = None, left_node = None, right_node = None, is_root = False, depth = 0, is_base = False):
+        self.value = value
+        self.Y = Y
+        self.feature = feature
+        self.threshold = threshold
+        self.left_node = left_node
+        self.right_node = right_node
+        self.is_base = is_base
 
-    def train(self, X_train, Y_train, modality = 'classification'):
-        self.X_train = X_train
-        self.Y_train = Y_train
-        self.modality = modality
-
-        if self.modality == 'classification':
-            self._train_classification(self.X_train, self.Y_train)
-    
-    def _train_classification(self, X, Y):
-        init_probs = self._log_odds(Y)
-        init_residuals = Y_train - init_probs
-        raw_output = self._transform_log_odds(init_residuals)
-
-    def _log_odds(self, Y):
-        labels, freqs = np.unique(Y, return_counts = True)
-        probs = freqs / Y.size
-        return np.log(probs / (1 - probs))
-
-    def _transform_residuals(self, residuals, probs):
-        return (np.sum(residuals) / np.sum(probs * (1 - probs))
-
-    def test(self, X_test, Y_test):
-        pass
-
+    def _is_leaf(self):
+        return self.value is not None
 
 if __name__ == "__main__":
     data = csv_to_numpy("data/DesTreeData.csv")
     train, test = train_test_split(data, train_split = .8)
     X_train, Y_train = x_y_split(train, y_col = 'last')
     X_test, Y_test = x_y_split(test, y_col = 'last')
-
+   
+    verbose_train = True
+    n_trees = 50
+    alpha = 1
+    dtree_dict = {
+        'verbose_train': True,
+        'verbose_test': True,
+        'criterion': 'gini',
+        'max_depth': 3,
+        'min_node_samples': 2,
+    }
     
-
+    model = GradientBoost(verbose_train = verbose_train)
+    model.train(X_train, Y_train, n_trees = n_trees, alpha = alpha, dtree_dict = dtree_dict)
 
     ''' 
     --- VALIDATING DECISION TREE CLASSIFICATION ---    
